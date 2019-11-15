69d05fa2d6394c0df0e24dec170a035b895ea775
sdusxw <sxw@sdboon.com>
2019-11-15 15:14:15 +0800

    openssl lock 4

--- src/common/pull_jpg2ram.cpp
+++ src/common/pull_jpg2ram.cpp
@@ -20,7 +20,7 @@
 void handle_error(const char *file, int lineno, const char *msg)
 {
     fprintf(stderr, "** %s:%d %s\n", file, lineno, msg);
-    ERR_print_errors_fp(stderr);
+    //ERR_print_errors_fp(stderr);
     /* exit(-1); */
 }
 
