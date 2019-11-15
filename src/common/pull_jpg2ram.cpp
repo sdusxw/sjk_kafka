266dc5c43165b98b2f880fef37499c23fe4d0e94
sdusxw <sxw@sdboon.com>
2019-11-15 14:52:28 +0800

    openssl lock

--- src/common/pull_jpg2ram.cpp
+++ src/common/pull_jpg2ram.cpp
@@ -8,6 +8,67 @@
 #include <iostream>
 #include <string>
 
+#include <openssl/err.h>
+
+#define MUTEX_TYPE       pthread_mutex_t
+#define MUTEX_SETUP(x)   pthread_mutex_init(&(x), NULL)
+#define MUTEX_CLEANUP(x) pthread_mutex_destroy(&(x))
+#define MUTEX_LOCK(x)    pthread_mutex_lock(&(x))
+#define MUTEX_UNLOCK(x)  pthread_mutex_unlock(&(x))
+#define THREAD_ID        pthread_self()
+
+void handle_error(const char *file, int lineno, const char *msg)
+{
+    fprintf(stderr, "** %s:%d %s\n", file, lineno, msg);
+    ERR_print_errors_fp(stderr);
+    /* exit(-1); */
+}
+
+/* This array will store all of the mutexes available to OpenSSL. */
+static MUTEX_TYPE *mutex_buf = NULL;
+
+static void locking_function(int mode, int n, const char *file, int line)
+{
+    if(mode & CRYPTO_LOCK)
+        MUTEX_LOCK(mutex_buf[n]);
+    else
+        MUTEX_UNLOCK(mutex_buf[n]);
+}
+
+static unsigned long id_function(void)
+{
+    return ((unsigned long)THREAD_ID);
+}
+
+int thread_setup(void)
+{
+    int i;
+    
+    mutex_buf = malloc(CRYPTO_num_locks() * sizeof(MUTEX_TYPE));
+    if(!mutex_buf)
+        return 0;
+    for(i = 0;  i < CRYPTO_num_locks();  i++)
+        MUTEX_SETUP(mutex_buf[i]);
+    CRYPTO_set_id_callback(id_function);
+    CRYPTO_set_locking_callback(locking_function);
+    return 1;
+}
+
+int thread_cleanup(void)
+{
+    int i;
+    
+    if(!mutex_buf)
+        return 0;
+    CRYPTO_set_id_callback(NULL);
+    CRYPTO_set_locking_callback(NULL);
+    for(i = 0;  i < CRYPTO_num_locks();  i++)
+        MUTEX_CLEANUP(mutex_buf[i]);
+    free(mutex_buf);
+    mutex_buf = NULL;
+    return 1;
+}
+
 //CURL函数的写内存回调函数
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb,
                                   void *userp)
@@ -45,6 +106,7 @@ bool JpgPuller::pull_image(char * url)
     char char_msg[100] = "";
     std::string log_msg = "";
     pthread_mutex_lock(&mutex_lock);
+    thread_setup();
     CURL *curl_handle;
     CURLcode res;
     chunk.memory = (char*) malloc(1); /* will be grown as needed by the realloc above */
@@ -104,6 +166,7 @@ bool JpgPuller::pull_image(char * url)
     curl_easy_cleanup(curl_handle);
     if (chunk.memory)
     free(chunk.memory);
+    thread_cleanup();
     pthread_mutex_unlock(&mutex_lock);
     if (!result)
     {
